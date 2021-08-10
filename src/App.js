0ca224c3901d31622693532c7a995176c346c125
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-08-09 18:22:26 -0700

    Completed project setup, task2 (up to create and connect a reducase case and action creator)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer, { initialState } from './reducers';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+//import addOne action
+import { addOne } from './actions';
+
+//import applyNumber action
+import { applyNumber, changeOperation } from './actions'
+
+//import changeOperation action
+//import { changeOperation } from './actions'
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state)
+
+  const addOneClick = () => {
+    dispatch(addOne());
+  }
+
+  const numberClick = (number) => () => {
+    dispatch(applyNumber(number))
+  }
+
+  const operatorClick= (symbol) => () => {
+    dispatch(changeOperation(symbol))
+  }
 
   return (
     <div className="App">
@@ -17,10 +42,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
             
             <div className="row">
@@ -30,27 +55,27 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={ numberClick(1) } />
+              <CalcButton value={2} onClick={ numberClick(2) }/>
+              <CalcButton value={3} onClick={ numberClick(3) }/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={ numberClick(4) }/>
+              <CalcButton value={5} onClick={ numberClick(5) }/>
+              <CalcButton value={6} onClick={ numberClick(6) }/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={ numberClick(7) }/>
+              <CalcButton value={8} onClick={ numberClick(8) }/>
+              <CalcButton value={9} onClick={ numberClick(9) }/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={ operatorClick("+")}/>
+              <CalcButton value={"*"} onClick={ operatorClick("*")}/>
+              <CalcButton value={"-"} onClick={ operatorClick("-")}/>
             </div>
 
             <div className="row ce_button">
--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,7 @@ export const addOne = () => {
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
 }
+
+export const changeOperation = (symbol) => {
+    return({type:CHANGE_OPERATION, payload: symbol});
+}
